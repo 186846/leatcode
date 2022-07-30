7a0fdbb8c850edb774054b0ca61511bfbdb0baac
186846 <93398636+186846@users.noreply.github.com>
2022-07-30 19:58:57 +0800

    Update RecentCounter.java

--- RecentCounter.java
+++ RecentCounter.java
@@ -1,18 +1,9 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
 
 class RecentCounter {
-    List<Integer> list = new ArrayList<>();
+    pubulic RecentCounter(){
+    }
     public int ping(int t) {
-        list.add(t);
-        int l = 0,r = list.size()-1;
-        while(l<r){
-            int m = r + l >> 1;
-            if(list.get(m)<t-3000) l = m + 1;
-            else r = m;
-        }
-        return list.size()-l;
     }
 }
